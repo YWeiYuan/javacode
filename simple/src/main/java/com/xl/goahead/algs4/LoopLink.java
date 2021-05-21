067633203a4a641f71051c5872542fa0540bbafa
huanglongquan <867184382@qq.com>
2021-05-21 10:27:09 +0800

    需求：并发编程 修改内容：1.Unsafe类的深入理解。

--- simple/src/main/java/com/xl/goahead/algs4/LoopLink.java
+++ simple/src/main/java/com/xl/goahead/algs4/LoopLink.java
@@ -23,6 +23,13 @@ public class LoopLink {
         }
         private Node next;
         private Integer value;
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    '}';
+        }
     }
 
     /**
@@ -47,18 +54,70 @@ public class LoopLink {
         return false;
     }
 
+    /**
+     *
+     * @param head
+     * @return
+     */
+    public static Node judgeRing(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node slow = head;
+        Node quick = head;
+        while (quick.next != null) {
+            quick = quick.next.next;
+            slow = slow.next;
+            if (slow == quick) {
+                quick = head;
+                while (quick != slow) {
+                    quick = quick.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+    /**
+     * 判断两个单链表是否相交并求交点
+     * @param node1
+     * @param node2
+     * @return
+     */
+    public static Node getJointNode(Node node1, Node node2) {
+        return null;
+    }
+
+
+
     public static void main(String[] args) {
         Node node1 = new Node(1);
         Node node2 = new Node(2);
         Node node3 = new Node(3);
         Node node4 = new Node(4);
         Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+        Node node8 = new Node(8);
+        Node node9 = new Node(9);
+        Node node10 = new Node(10);
         node1.setNext(node2);
         node2.setNext(node3);
         node3.setNext(node4);
         node4.setNext(node5);
-        node5.setNext(node4);
+        node5.setNext(node6);
+        node6.setNext(node7);
+        node7.setNext(node8);
+        node8.setNext(node9);
+        node9.setNext(node10);
+        node10.setNext(node5);
         System.out.println(hasLoop(node1));
+        Node ring = judgeRing(node1);
+        if (ring != null) {
+            System.out.println(ring.getValue());
+        }
+
     }
 
 }
---
+++ simple/src/main/java/com/xl/goahead/juc/unsafe/UnsafeTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2012-2022
+ */
+package com.xl.goahead.juc.unsafe;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * Unsafe类的深入理解
+ * 可以用来在任意内存地址位置处读写数据，可见，对于普通用户来说，使用起来还是比较危险的
+ * @author longquan.huang
+ * @version 1.0
+ * @date 2021/5/20 11:13 上午
+ */
+public class UnsafeTest {
+    public static void main(String[] args) {
+        allocateInstance();
+    }
+
+
+    /**
+     * 获取unsafe对象,不能通过new Unsafe 以及 getUnsafe()方法获取，
+     * 因为 unsafe被设计成单利模式，并且只能通过类加载器进行调用 可以查看源码 Unsafe.getUnsafe()的实现
+     * 我们可以通过反射来加载，Unsafe类有个私有静态变量theUnsafe
+     * @return unsafe对象
+     */
+    private static Unsafe getUnsafeByReflect() {
+        try {
+            // 通过反射得到theUnsafe对应的对象
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            // 设置field 可以访问
+            theUnsafe.setAccessible(true);
+            // 通过Field得到该Field对应的对象，传入null是因为该Field是static
+            return  (Unsafe) theUnsafe.get(null);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * unsafe allocateInstance方法，不调用构造方法生成对象
+     */
+    public static void allocateInstance() {
+        Unsafe unsafe = getUnsafeByReflect();
+        Test1 test1 = new Test1();
+        System.out.println(test1.getA());
+        try {
+            Test1 test11 = Test1.class.newInstance();
+            Integer a = test11.getA();
+            System.out.println(a);
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (unsafe != null) {
+                Test1 o = (Test1)unsafe.allocateInstance(Test1.class);
+                System.out.println(o.getA());
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+
+    }
+    static class Test1 {
+        private Integer a = 0;
+
+        public Test1() {
+            this.a = 1;
+        }
+
+        public Integer getA() {
+            return a;
+        }
+    }
+
+
+
+}
