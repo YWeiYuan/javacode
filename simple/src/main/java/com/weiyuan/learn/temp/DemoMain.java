2c22dd288aa176e198f3a3709416445b1400e2d8
longquan.huang <longquan.huang@ucarinc.com>
2021-09-16 23:14:20 +0800

    模块：JMM 内存模型 修改点：1.new 一个对象在内存中做了什么？

---
+++ simple/src/main/java/com/weiyuan/learn/temp/DemoMain.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2012-2022
+ */
+package com.weiyuan.learn.temp;
+
+/**
+ * Demo demo = new Demo() 在内存做什么？
+ *
+ 1.加载Demo.class 文件进内存（方法区）
+ 2.在栈空间为Demo demo变量开辟空间
+ 3.在堆空间为对象 new Demo()申请一个空间
+ 4.对象成员变量默认初始化，null，0
+ 5.对象成员变量进行显示初始化，longquan.huang，27
+ 6.执行构造方法给成员变量进行初始化，longquan，30
+ 7.对象初始化完毕，把堆内存地址赋值给栈内存中的demo变量
+ * @author longquan.huang
+ * @version 1.0
+ * @date 2021/9/16 11:03 下午
+ */
+public class DemoMain {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+    }
+}
+
+class Demo {
+    private String name = "longquan.huang";
+    private int age = 27;
+
+    public Demo() {
+        this.name = "longquan";
+        this.age = 30;
+    }
+}
